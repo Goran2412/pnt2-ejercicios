b52dc33995e869aa0cc3781740ec5b58cfffef89
Goran Westberg <gorandirkwestberg@gmail.com>
2023-04-03 22:47:18 -0300

    resolucion del ejercicio uno

---
+++ EjercicioUno.js
@@ -0,0 +1,24 @@
+const personajesSimpsons = [
+    { nombre: "Homero", edad: 39, rol: "Padre" },
+    { nombre: "Marge", edad: 36, rol: "Madre" },
+    { nombre: "Bart", edad: 10, rol: "Hijo" },
+    { nombre: "Lisa", edad: 8, rol: "Hija" },
+    { nombre: "Maggie", edad: 1, rol: "Hija" },
+    { nombre: "Ned", edad: 35, rol: "Vecino" },
+    { nombre: "Ralph", edad: 8, rol: "Compañero de clase" },
+    { nombre: "Milhouse", edad: 10, rol: "Amigo" },
+    { nombre: "Nelson", edad: 12, rol: "Bravucón" },
+    { nombre: "Martin", edad: 10, rol: "Compañero de clase" }
+    ];
+
+    // Desarrollo del ejercicio a partir de acá.
+
+/*Ejercicio 1: Filtrar personajes menores de edad
+Crear un array de objetos con información de personajes de Los Simpsons, cada
+objeto debe contener el nombre, la edad y el rol del personaje. Utilizar el método
+filter para obtener un nuevo array que contenga solamente los personajes
+menores de 18 años.*/
+
+const menoresADieciocho = personajesSimpsons.filter(pj => pj.edad < 18);
+
+console.log(menoresADieciocho);
